2ca2360eee7e181b5cd635b6cf28ce8c3897daa1
Mel Grosser <grosser.mel@live.de>
2021-04-27 12:32:38 +0200

    JS Intro exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import AppHeader from './components/AppHeader'
+console.clear()
+const buttonAdd = document.querySelector('.button-add')
+const buttonRemove = document.querySelector('.button-remove')
+const buttonToggle = document.querySelector('.button-toggle')
+const box = document.querySelector('.box')
 
-document.body.append(AppHeader('JavaScript App Template'))
+buttonAdd.addEventListener('click', () => {
+  box.classList.add('blue')
+})
+
+buttonRemove.addEventListener('click', () => {
+  box.classList.remove('blue')
+})
+
+buttonToggle.addEventListener('click', () => {
+  box.classList.toggle('blue')
+})
