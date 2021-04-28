917171c11fd2fda4c7ec88ec447c0f1ce072f46c
Mel Grosser <grosser.mel@live.de>
2021-04-28 15:24:16 +0200

    c

--- src/index.js
+++ src/index.js
@@ -1,58 +1,10 @@
-console.clear()
-const buttonOne = document.querySelector('.button1')
-const buttonTwo = document.querySelector('.button2')
-const buttonThree = document.querySelector('.button3')
-const buttonFour = document.querySelector('.button4')
+const box1 = document.querySelector('[name="box1"]')
+const box2 = document.querySelector('[name="box2"]')
+const button = document.querySelector('[data-button]')
+let one = ''
 
-const box1 = document.querySelector('.box1')
-const box2 = document.querySelector('.box2')
-const box3 = document.querySelector('.box3')
-const box4 = document.querySelector('.box4')
-
-buttonOne.addEventListener('click', () => {
-  box1.classList.add('visible')
-  box2.classList.remove('visible')
-  box3.classList.remove('visible')
-  box4.classList.remove('visible')
-
-  buttonOne.classList.add('clicked')
-  buttonTwo.classList.remove('clicked')
-  buttonThree.classList.remove('clicked')
-  buttonFour.classList.remove('clicked')
-})
-
-buttonTwo.addEventListener('click', () => {
-  box1.classList.remove('visible')
-  box2.classList.add('visible')
-  box3.classList.remove('visible')
-  box4.classList.remove('visible')
-
-  buttonOne.classList.remove('clicked')
-  buttonTwo.classList.add('clicked')
-  buttonThree.classList.remove('clicked')
-  buttonFour.classList.remove('clicked')
-})
-
-buttonThree.addEventListener('click', () => {
-  box1.classList.remove('visible')
-  box2.classList.remove('visible')
-  box3.classList.add('visible')
-  box4.classList.remove('visible')
-
-  buttonOne.classList.remove('clicked')
-  buttonTwo.classList.remove('clicked')
-  buttonThree.classList.add('clicked')
-  buttonFour.classList.remove('clicked')
-})
-
-buttonFour.addEventListener('click', () => {
-  box1.classList.remove('visible')
-  box2.classList.remove('visible')
-  box3.classList.remove('visible')
-  box4.classList.add('visible')
-
-  buttonOne.classList.remove('clicked')
-  buttonTwo.classList.remove('clicked')
-  buttonThree.classList.remove('clicked')
-  buttonFour.classList.add('clicked')
+button.addEventListener('click', () => {
+  one = box1.value
+  box1.value = box2.value.toUpperCase()
+  box2.value = one.toLowerCase()
 })
