0072c2294b1b049fcf8c94f13c7de54df05b961d
Mel Grosser <grosser.mel@live.de>
2021-04-28 15:59:53 +0200

    comit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const box1 = document.querySelector('[name="box1"]')
-const box2 = document.querySelector('[name="box2"]')
-const button = document.querySelector('[data-button]')
-let one = ''
+const box = document.querySelector('[data-box]')
+const input = document.querySelector('[name="input-color"]')
+const form = document.querySelector('[data-form]')
 
-button.addEventListener('click', () => {
-  one = box1.value
-  box1.value = box2.value.toUpperCase()
-  box2.value = one.toLowerCase()
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  box.style.background = input.value
+  input.value = ''
+  input.focus()
 })
